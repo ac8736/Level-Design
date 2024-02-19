39ae17e6c9a85c0019bd182dcdafad66bfb00ec7
Kewl <87680132+ac8736@users.noreply.github.com>
2024-02-19 17:00:19 -0500

    Door logic

---
+++ Assets/Script/Door.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    public List<PressurePlate> m_Plates;
+    public string m_NextScene;
+
+    private bool m_IsOpen = false;
+    private Animator m_Animator;
+    private bool canOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        canOpen = CheckAllPlatesPressed();
+
+        if (canOpen && !m_IsOpen)
+        {
+            m_IsOpen = true;
+            m_Animator.SetTrigger("Open");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (canOpen && collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(m_NextScene);
+        }
+    }
+
+    private bool CheckAllPlatesPressed()
+    {
+        foreach (var plate in m_Plates)
+        {
+            if (!plate.pressed)
+            {
+                if (m_IsOpen)
+                {
+                    m_IsOpen = false;
+                    m_Animator.SetTrigger("Close");
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+}
